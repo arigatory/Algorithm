ac551d14618270e5a17bbc657e7a7e26ea09c472
Ivan MSI <arigatory@gmail.com>
2022-03-02 00:11:32 +0300

    code runs but WA

---
+++ ToDoList/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class Node<TValue>
+{
+    public TValue Value { get; private set; }
+    public Node<TValue> Next { get; set; }
+
+    public Node(TValue value, Node<TValue> next)
+    {
+        Value = value;
+        Next = next;
+    }
+}
+
+public class Solution
+{
+    private static TextReader _reader;
+    private static TextWriter _writer;
+
+    public static void Main(string[] args)
+    {
+        InitialiseStreams();
+
+        //var n = ReadInt();
+        //var numbers = ReadList();
+
+        //for (var i = 0; i < n; i++)
+        //{
+        //    _writer.Write("{0} ", numbers[i]);
+        //}
+
+        Test();
+
+        CloseStreams();
+    }
+
+
+    private static void Test()
+    {
+        var node3 = new Node<string>("node3", null);
+        var node2 = new Node<string>("node2", node3);
+        var node1 = new Node<string>("node1", node2);
+        var node0 = new Node<string>("node0", node1);
+        //Solve(node0);
+        /*
+        Output is:
+        node0
+        node1
+        node2
+        node3
+        */
+    }
+
+
+    public static void Solve(Node<string> head)
+    {
+        var start = head;
+        while (head != null)
+        {
+            Console.WriteLine(head.Value);
+            head = head.Next;
+        }
+    }
+
+    
+    private static void CloseStreams()
+    {
+        _reader.Close();
+        _writer.Close();
+    }
+
+    private static void InitialiseStreams()
+    {
+        _reader = new StreamReader(Console.OpenStandardInput());
+        _writer = new StreamWriter(Console.OpenStandardOutput());
+    }
+
+    private static int ReadInt()
+    {
+        return int.Parse(_reader.ReadLine());
+    }
+
+    private static List<int> ReadList()
+    {
+        return _reader.ReadLine()
+            .Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+    }
+}
+
