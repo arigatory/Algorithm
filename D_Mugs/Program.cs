34912531c6c738b2c48b5af1e989e54f405ce9c9
Ivan MSI <arigatory@gmail.com>
2022-03-29 23:30:48 +0300

    4D done

---
+++ D_Mugs/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace D_Mugs
+{
+    public class Solution
+    {
+        private static TextReader _reader;
+        private static TextWriter _writer;
+
+        public static void Main(string[] args)
+        {
+            InitialiseStreams();
+
+            var n = ReadInt();
+
+            SortedSet<string> set = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var s = _reader.ReadLine();
+                if (!set.Contains(s))
+                {
+                    _writer.WriteLine(s);
+                    set.Add(s);
+                }
+
+            }
+            CloseStreams();
+        }
+
+        private static void CloseStreams()
+        {
+            _reader.Close();
+            _writer.Close();
+        }
+
+        private static void InitialiseStreams()
+        {
+            _reader = new StreamReader(Console.OpenStandardInput());
+            _writer = new StreamWriter(Console.OpenStandardOutput());
+        }
+
+        private static int ReadInt()
+        {
+            return int.Parse(_reader.ReadLine());
+        }
+
+        private static List<int> ReadList()
+        {
+            return _reader.ReadLine()
+                .Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+        }
+    }
+
+}
