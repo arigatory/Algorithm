a9e85ce962cd52a211a3ae5ae162459ea82ee8b4
Ivan MSI <arigatory@gmail.com>
2022-04-08 23:40:10 +0300

    4J done

--- J_SumOfFours/Program.cs
+++ J_SumOfFours/Program.cs
@@ -18,38 +18,87 @@ namespace J_SumOfFours
             var n = ReadInt();
             var s = ReadInt();
             var numbers = ReadList();
-            numbers.Sort();
 
-            var fours = new SortedSet<(int, int, int, int)>();
-            var history = new HashSet<int>();
+            var result = FourSum(numbers, s);
 
-            for (var i = 0; i < n; i++)
+            _writer.WriteLine(result.Count);
+
+            foreach (var item in result)
             {
-                for (int j = i + 1; j < n; j++)
-                {
-                    for (int k = j + 1; k < n; k++)
-                    {
-                        var target = s - numbers[i] - numbers[j] - numbers[k];
-                        if (history.Contains(target))
-                        {
-                            fours.Add((target, numbers[i], numbers[j], numbers[k]));
-                        }
-                    }
-                    
-                }
-                history.Add(numbers[i]);
-            }
-
-            _writer.WriteLine(fours.Count);
-            foreach (var item in fours)
-            {
-                _writer.WriteLine($"{item.Item1} {item.Item2} {item.Item3} {item.Item4}");
+                _writer.WriteLine(string.Join(" ", item));
             }
 
             CloseStreams();
         }
 
-private static void CloseStreams()
+        static List<List<int>> FourSum(List<int> nums, int target)
+        {
+            nums.Sort();
+            return kSum(nums, target, 0, 4);
+        }
+
+        static private List<List<int>> kSum(List<int> nums, int target, int start, int k)
+        {
+            List<List<int>> result = new List<List<int>>();
+            if (start == nums.Count)
+            {
+                return result;
+            }
+
+            int averageValue = target / k;
+
+            if (nums[start] > averageValue || averageValue > nums.Last())
+            {
+                return result;
+            }
+
+            if (k == 2)
+            {
+                return TwoSum(nums, target, start);
+            }
+
+            for (int i = start; i < nums.Count; i++)
+            {
+                if (i == start || nums[i - 1] != nums[i])
+                {
+                    foreach (var subset in kSum(nums, target - nums[i], i + 1, k - 1))
+                    {
+                        var list = new List<int>();
+                        list.Add(nums[i]);
+                        list.AddRange(subset);
+                        result.Add(list);
+                    }
+                }
+            }
+            return result;
+        }
+
+        static private List<List<int>> TwoSum(List<int> nums, int target, int start)
+        {
+            var result = new List<List<int>>();
+            int lo = start;
+            int hi = nums.Count - 1;
+
+            while (lo < hi)
+            {
+                int currSum = nums[lo] + nums[hi];
+                if (currSum < target || (lo > start && nums[lo] == nums[lo - 1]))
+                {
+                    ++lo;
+                }
+                else if (currSum > target || (hi < nums.Count - 1 && nums[hi] == nums[hi + 1]))
+                {
+                    --hi;
+                }
+                else
+                {
+                    result.Add(new List<int> { nums[lo++], nums[hi--] });
+                }
+            }
+            return result;
+        }
+
+        private static void CloseStreams()
         {
             _reader.Close();
             _writer.Close();
