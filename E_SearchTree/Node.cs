e86df272d9e4d637c5c1d08f698723c737de3cf4
Ivan MSI <arigatory@gmail.com>
2022-04-29 05:34:07 +0300

    5E done

---
+++ E_SearchTree/Node.cs
@@ -0,0 +1,16 @@
+﻿namespace E_SearchTree
+{
+    public class Node
+    {
+        public int Value;
+        public Node Left;
+        public Node Right;
+
+        public Node(int value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+        }
+    }
+}
