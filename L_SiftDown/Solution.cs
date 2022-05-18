dac09250a7674cdd8350b0f698cfb48dcd11f039
Ivan MSI <arigatory@gmail.com>
2022-05-18 03:42:26 +0300

    5L fix

--- L_SiftDown/Solution.cs
+++ L_SiftDown/Solution.cs
@@ -1,30 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-//namespace L_SiftDown
-//{
-public class Solution
+namespace L_SiftDown
 {
-    public static int SiftDown(List<int> array, int idx)
+    public class Solution
     {
-        int left = 2 * idx;
-        int right = 2 * idx + 1;
-
-        if (array.Count < left)
+        public static int SiftDown(List<int> array, int idx)
         {
-            return idx;
-        }
+            int left = 2 * idx;
+            int right = 2 * idx + 1;
 
-        
-        int indexLargest;
+            if (array.Count <= left)
+            {
+                return idx;
+            }
 
-        // both 
-        if (right <= array.Count)
-        {
-            if (array[left] < array[right])
+
+            int indexLargest;
+
+            // both 
+            if (right < array.Count && array[left] < array[right])
             {
                 indexLargest = right;
             }
@@ -32,30 +26,21 @@ public class Solution
             {
                 indexLargest = left;
             }
-        }
-        else
-        {
-            // only left
-            indexLargest = left;
-        }
 
-        if (array[idx] < array[indexLargest])
-        {
-            var temp = array[idx];
-            array[idx] = array[indexLargest];
-            array[indexLargest] = temp;
-            return SiftDown(array, indexLargest);
-        }
-        else
-        {
+            if (array[idx] < array[indexLargest])
+            {
+                var temp = array[idx];
+                array[idx] = array[indexLargest];
+                array[indexLargest] = temp;
+                return SiftDown(array, indexLargest);
+            }
             return idx;
         }
-    }
 
-    public static void Test()
-    {
-        var sample = new List<int> { -1, 12, 1, 8, 3, 4, 7 };
-        System.Console.WriteLine(SiftDown(sample, 2) == 5);
+        public static void Test()
+        {
+            var sample = new List<int> { -1, 12, 1, 8, 3, 4, 7 };
+            System.Console.WriteLine(SiftDown(sample, 2) == 5);
+        }
     }
 }
-//}
