3b64df2175ee6d687c5a88c0c736dc57c84f287a
Ivan MSI <arigatory@gmail.com>
2022-06-18 15:29:50 +0300

    8A and 0,1 folders

---
+++ 8/A_StringReversal/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace A_StringReversal
+{
+    public class Solution
+    {
+        private static TextReader _reader;
+        private static TextWriter _writer;
+
+        public static void Main(string[] args)
+        {
+            InitialiseStreams();
+
+            var s = _reader.ReadLine().Split(' ').Reverse();
+            _writer.WriteLine(string.Join(' ', s));
+
+            CloseStreams();
+        }
+
+        private static void CloseStreams()
+        {
+            _reader.Close();
+            _writer.Close();
+        }
+
+        private static void InitialiseStreams()
+        {
+            _reader = new StreamReader(Console.OpenStandardInput());
+            _writer = new StreamWriter(Console.OpenStandardOutput());
+        }
+    }
+}
