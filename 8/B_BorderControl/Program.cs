baa33e8822ea8a2fd72be387f851263cd5c2160f
Ivan MSI <arigatory@gmail.com>
2022-06-19 07:43:44 +0300

    fix 8B

--- 8/B_BorderControl/Program.cs
+++ 8/B_BorderControl/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace B_BorderControl
 {
@@ -37,20 +35,17 @@ namespace B_BorderControl
                 var temp = s1;
                 s1 = s2;
                 s2 = temp;
-                var x = n;
-                n = m;
-                m = x;
             }
 
             int n = s1.Length;
             int m = s2.Length;
 
-            if (n-m > 1)
+            if (n - m > 1)
             {
                 return false;
             }
             else if (m == n)
-                {
+            {
                 int i = 0;
                 int errCount = 0;
                 while (i < m)
@@ -58,33 +53,30 @@ namespace B_BorderControl
                     if (s1[i] != s2[i])
                     {
                         errCount++;
-                        if (errCount>1)
-                    {
+                        if (errCount > 1)
+                        {
                             return false;
                         }
                     }
                     i++;
-                    }
+                }
                 return true;
-            } else
+            }
+            else
             {
                 int i = 0;
                 int delta = 0;
                 int errCount = 0;
                 while (i < m)
                 {
-                    if (s1[i+delta] != s2[i])
+                    if (s1[i + delta] != s2[i])
                     {
                         errCount++;
                         delta++;
                         if (errCount > 1)
-                    {
+                        {
                             return false;
-                    }
-                    else
-                    {
-                        int temp = Math.Min(dp[i & 1, j - 1], dp[(i - 1) & 1, j]);
-                        dp[i & 1, j] = Math.Min(temp + 1, dp[(i - 1) & 1, j - 1] + (s1[i - 1] == s2[j - 1] ? 0 : 1));
+                        }
                     }
                     i++;
                 }
