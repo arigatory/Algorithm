cbdedea0ff2dffbf910c8feb0da6307152fe9861
Ivan MSI <arigatory@gmail.com>
2022-06-22 20:53:16 +0300

    8H done

---
+++ 8/H_GlobalReplace/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace H_GlobalReplace
+{
+    public class Solution
+    {
+        private static TextReader _reader;
+        private static TextWriter _writer;
+
+        public static void Main(string[] args)
+        {
+            InitialiseStreams();
+
+            StringBuilder s = new StringBuilder(_reader.ReadLine());
+            string pattern = _reader.ReadLine();
+            string res = _reader.ReadLine();
+
+            s.Replace(pattern, res);
+
+            _writer.WriteLine(s);
+
+            CloseStreams();
+        }
+
+        private static void CloseStreams()
+        {
+            _reader.Close();
+            _writer.Close();
+        }
+
+        private static void InitialiseStreams()
+        {
+            _reader = new StreamReader(Console.OpenStandardInput());
+            _writer = new StreamWriter(Console.OpenStandardOutput());
+        }
+    }
+}
